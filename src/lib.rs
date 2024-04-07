2d9e90f779639cb0c637af4beeb6be92bf5ad170
zhangyushao <ling0017@outlook.com>
2024-04-07 13:01:25 +0800

    Add support for initializing the library with a C string

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,20 @@ mod dfa;
 mod lexer;
 mod nfa;
 
+use std::ffi::CStr;
+use std::os::raw::c_char;
+
 #[no_mangle]
-pub extern "C" fn init() {
-    println!("init");
+pub extern "C" fn init(c_string: *const c_char) {
+    // 安全地将C字符串转换为Rust字符串切片
+    let c_str = unsafe {
+        assert!(!c_string.is_null());
+        CStr::from_ptr(c_string)
+    };
+
+    // 将CStr转换为Rust的字符串切片，如果包含有效的UTF-8数据
+    match c_str.to_str() {
+        Ok(str_slice) => println!("Received string: {}", str_slice),
+        Err(e) => eprintln!("Failed to convert C string to Rust string: {:?}", e),
+    }
 }
